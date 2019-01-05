c35e8a6dc9d1b5f1685d1f2653d4cb4893080700
Max Arnold <arnold.maxim@gmail.com>
2019-01-05 16:55:32 +0700

    Reformat with black

--- mailchimp-backup.py
+++ mailchimp-backup.py
@@ -36,6 +36,7 @@ def _filename(out, lst):
     }
     return os.path.abspath(out.format(**attrs))
 
+
 _filename.now = None
 
 
@@ -125,7 +126,11 @@ if __name__ == '__main__':
     parser.add_argument('--list', type=str, help='List ID')
     parser.add_argument('--all-lists', action='store_true', help='List ID')
     parser.add_argument('--out', type=str, help='Output file')
-    parser.add_argument('--fail-if-empty', action='store_true', help='Fail if there are no lists or any of them is empty')
+    parser.add_argument(
+        '--fail-if-empty',
+        action='store_true',
+        help='Fail if there are no lists or any of them is empty',
+    )
     options = parser.parse_args()
 
     key = options.key or os.environ.get('MAILCHIMP_KEY')
@@ -146,8 +151,7 @@ if __name__ == '__main__':
         lst = export_list(key, options.list)
         if options.fail_if_empty and len(lst.split('\n')) < 2:
             parser.exit(
-                status=1,
-                message='List {} is empty'.format(options.list)
+                status=1, message='List {} is empty'.format(options.list)
             )
         if options.out:
             filename = _filename(options.out, options.list)
@@ -162,16 +166,10 @@ if __name__ == '__main__':
         current_filename = None
         lists = list(export_all_lists(key, options))
         if options.fail_if_empty and len(lists) == 0:
-                parser.exit(
-                    status=1,
-                    message='No lists found'
-                )
+            parser.exit(status=1, message='No lists found')
         for lst_id, lst in lists:
             if options.fail_if_empty and len(lst.split('\n')) < 2:
-                parser.exit(
-                    status=1,
-                    message='List {} is empty'.format(lst_id)
-                )
+                parser.exit(status=1, message='List {} is empty'.format(lst_id))
             if options.out:
                 filename = _filename(options.out, lst_id)
                 os.makedirs(os.path.dirname(filename), exist_ok=True)
